66e00979d14b57a4f7edae9a70a8074c4d4c7a74
Manik023sangal <145379834+Manik023sangal@users.noreply.github.com>
2024-10-11 23:05:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a =7;
+        int b=6;
+        System.out.println(a+b);
+
+    }
+}
